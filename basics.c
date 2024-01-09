f68a4ff33b8b8dd1a52f1335e6714b138011708b
Marco <sanchez.junco@gmail.com>
2024-01-09 10:22:55 -0800

    M&R book intro

---
+++ basics.c
@@ -0,0 +1,19 @@
+// Compile the C code:
+
+// gcc basics.c -o basics
+// This command compiles hello.c and produces an executable named basics.
+
+// Run the executable:
+// After compilation, you can run the executable from the terminal.
+
+// Example:
+
+// ./basics
+
+#include <stdio.h>
+
+int main (void) {
+    printf("\n Hello World \n");
+
+    return 0;
+}
